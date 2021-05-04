233d59e08a75e4f2135be9106a5e0f8be46b7f24
HelenDruzhinskaya <alena1806@mail.ru>
2021-05-05 01:19:27 +0500

    Initial

--- app/src/main/java/ru/myitacademy/samsung/threereceivers/MainActivity.kt
+++ app/src/main/java/ru/myitacademy/samsung/threereceivers/MainActivity.kt
@@ -5,13 +5,27 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val r1 = MyReceiver1()
+    val r2 = MyReceiver2()
+    val r3 = MyReceiver1()
+    val r4 = MyReceiver3()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
         val filter = IntentFilter("ru.myitacademy.samsung.advertising.POST")
-        registerReceiver(MyReceiver1(),filter.apply { priority = 17 })
-        registerReceiver(MyReceiver2(),filter.apply { priority = -7 })
-        registerReceiver(MyReceiver1(),filter.apply { priority = 0 })
-        registerReceiver(MyReceiver3(),filter.apply { priority = 7 })
+        registerReceiver(r1, filter.apply { priority = 17 })
+        registerReceiver(r2, filter.apply { priority = -7 })
+        registerReceiver(r3, filter.apply { priority = 0 })
+        registerReceiver(r4, filter.apply { priority = 7 })
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        unregisterReceiver(r1)
+        unregisterReceiver(r2)
+        unregisterReceiver(r3)
+        unregisterReceiver(r4)
     }
 }
